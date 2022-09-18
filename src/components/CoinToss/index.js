1d9f4a2c30585627409aa08e2a1e579ee3dbd4f3
Vipin Patel <112804686+Vipin-patelME@users.noreply.github.com>
2022-09-18 17:06:37 +0530

    Update index.js

--- src/components/CoinToss/index.js
+++ src/components/CoinToss/index.js
@@ -7,22 +7,50 @@ class CoinToss extends Component {
     Total: 0,
     Heads: 0,
     Tails: 0,
+    number: 0,
+  }
+
+  onHeadsCount = () => {
+    this.setState(prevState => ({Heads: prevState.Heads + 1}))
+  }
+
+  onToss = () => {
+    const coinnumber = Math.round(Math.random() * 100)
+    this.setState(prevState => ({
+      number: coinnumber,
+      Total: prevState.Total + 1,
+    }))
   }
 
   render() {
-    const {Total, Heads, Tails} = this.state
+    const {Total, Heads, Tails, number} = this.state
+    let image
+    if (number % 2 === 0) {
+      image = (
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/heads-img.png"
+          className="coin-image"
+          alt="toss result"
+        />
+      )
+    } else {
+      image = (
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/tails-img.png"
+          className="coin-image"
+          alt="toss result"
+        />
+      )
+    }
+
     return (
       <div className="bg-container">
         <div className="card-container">
-          <h1 className="card-hading">Coins Toss Game </h1>
+          <h1 className="card-hading">Coin Toss Game </h1>
           <p className="card-para">Heads (or) Tails</p>
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/heads-img.png"
-            className="coin-image"
-            alt="coin"
-          />
+          {image}
           <br />
-          <button className="btn-style" type="button">
+          <button className="btn-style" type="button" onClick={this.onToss}>
             Toss Coins
           </button>
           <p>{`Total: ${Total}     Heads: ${Heads}   Tails: ${Tails}`}</p>
